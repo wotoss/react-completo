f9da04fecfe281cd46b1bfc1ae97f07ca38106a1
wotoss <wotoss10@gmail.com>
2021-12-31 09:06:50 -0500

    segunda atividade mexendo na em pastas strutura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props){
+       super.state = {
+           nome: 'Matheus',
+           contador: 0
+       };
+      this.aumentar = this.aumentar.bind(this);
+      this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador += 1;
+        state.nome = 'José';
+        console.setState(state);
+    }
+    diminuir(){
+        let state = this.state;
+        if (state.contador === 0){
+            alert('Opá chegou a zero');
+            return;
+        }
+        state.contador -= 1;
+        this.setState(state);
+    }
+
+    render(){
+        return(
+           <div>
+             <h1>Contador</h1>
+             {this.state.nome}
+             <button onClick={this.diminuir}>-</button>
+             <h3>{this.state.contador}</h3>
+             <button onClick={this.aumentar}>+</button>
+           </div>
+        );
+    }
 }
-
 export default App;
