8884d026c88cf49e144d62e91310b549617f8610
wotoss <wotoss10@gmail.com>
2021-12-31 12:15:54 -0500

    renderizacao condicional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-import Membro from "./componentes/Membro";
+
 
 
 class App extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            status: false
+        };
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
+    }
+    entrar(){
+        this.setState({status: true});
+    }
+    sair(){
+       this.setState({status: false});
+    }
+
     render(){
         return(
            <div>
-             <Membro nome="Visitante" />            
+              {this.state.status ?
+                   <div>
+                      <h2>Bem-vindo ao sistema!</h2>
+                      <button onClick={this.sair}>Sair</button>
+                   </div> :             
+                   <div>
+                      <h2>Olá visitante, faça o login!</h2>
+                      <button onClick={this.entrar}>Entrar no sistema</button>
+                   </div>
+             }
            </div>
         );
     }
